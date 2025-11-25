6ec5a14230efa1720ca0c569a2b9dc62e595f9fb
Joel Kiptoo <deusjoel150@gmail.com>
2025-11-25 11:22:05 +0300

    Standalone Asio removed the older Boost style method.

--- src/carrierbridge/include/carrierbridge.h
+++ src/carrierbridge/include/carrierbridge.h
@@ -1,21 +1,23 @@
 #pragma once
 #include <string>
 #include <functional>
-#include <unordered_map>
 
 class CBServer {
 public:
     using MessageCallback = std::function<void(const std::string& from, const std::string& message)>;
 
-    CBServer(unsigned short port = 9000); // bind UDP port
+    explicit CBServer(unsigned short port = 9000);
     ~CBServer();
 
+    // start / stop
     void init();
     void shutdown();
 
+    // client operations (client-side helpers that send to the broker)
     void register_user(const std::string& username);
     void send_message(const std::string& to, const std::string& message);
 
+    // broker-side callback when a message is routed to local process
     void set_message_callback(MessageCallback cb);
 
 private:
